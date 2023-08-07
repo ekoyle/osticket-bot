aa53544b3ecf232a0e1352d842c5a5a30b676295
Nemus <nemus@Nemuss-MacBook-Pro-2.local>
2023-08-07 15:09:14 -0700

    updating run.sh

--- build/run.sh
+++ build/run.sh
@@ -5,21 +5,18 @@ HOST='osticket_bot'
 PROJECT='osticket_bot'
 DOCKER_TAG='bhnoc/osticket_bot:latest'
 CONTAINER='osticket_bot'
-LOG_PATH='/home/bhnoc/logs'
-mkdir -p $LOG_PATH/$CONTAINER/
-APP_PATH='/home/bhnoc/git/base/docker/'
+APP_PATH='/Users/nemus/Desktop/git'
+mkdir -p $APP_PATH/docker/configs/$CONTAINER
 docker stop osticket_bot
 docker rm osticket_bot
 docker run -d \
-	--add-host=osticket_bot.fuzeflow.com:127.0.0.1 \
-	-p8200:8200 \
+	--add-host=osticket_bot.bhnoc.org:127.0.0.1 \
 	--cap-add=IPC_LOCK \
 	--name osticket_bot \
 	--network bhnoc \
 	-h osticket_bot \
 	--restart unless-stopped \
-	-v $APP_PATH/logs/osticket_bot:/var/log/osticket_bot/ \
-	-v $APP_PATH/configs/$CONTAINER/config:/opt/env/live/config \
-	-v $APP_PATH/configs/$CONTAINER/logs:/opt/env/live/logs \
+	-v $APP_PATH/containers/$CONTAINER/config:/opt/env/live/config \
+	-v $APP_PATH/containers/$CONTAINER/logs:/opt/env/live/logs \
 	-v $APP_PATH/$CONTAINER/app:/opt/env/live/app \
 	$DOCKER_TAG
